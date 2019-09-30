12501c2c44ab0f9289294e9a17aa6c281d4d7d17
Arne Molland <arneolaim@gmail.com>
2019-09-30 17:35:46 +0200

    🎉 Initial commit

---
+++ src/main/java/io/roger/refactoring/InlineMethodExercise.java
@@ -0,0 +1,31 @@
+package io.roger.refactoring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class InlineMethodExercise {
+
+	static class A {
+		int foo() {
+			return 1;
+		}
+
+		public int doIt() {
+			int result = foo();
+			System.out.println(result);
+			return result;
+		}
+	}
+
+	static class B extends A {
+		int foo() {
+			return 2;
+		}
+	}
+
+	@Test
+	public void test() {
+		assertEquals(2, new B().doIt());
+	}
+}
